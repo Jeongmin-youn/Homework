27bc5be518b659a78c4271ab78cd1957ca1a44ea
Jeongmin-youn <wjdals040315@cau.ac.kr>
2025-05-01 00:34:33 +0900

    3-19.js: String도 array-like, map은 새로운 array를 만듦

---
+++ Homework1/3-19.js
@@ -0,0 +1,19 @@
+var str = 'abc def';
+
+Array.prototype.push.call(str, ', pushed string');
+// Error: Cannot assign to read only property 'length' of object [object String]
+
+Array.prototype.concat.call(str, 'string');
+
+Array.prototype.every.call(str, function(c) { return char !== ' '});
+
+Array.prototype.some.call(str, function(c) { return char !== ' '});
+
+var newArr = Array.prototype.map.call(str, function(c) { return char + '!'; });
+console.log(newArr);
+
+var newStr = Array.prototype.reduce.apply(str, [
+    function(string, char, i) { return string + char + i; },
+    ''
+]);
+console.log(newStr);
